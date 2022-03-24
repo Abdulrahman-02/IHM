22aad18e6564d577df827d287895051a7df183fb
paravoke <66128724+paravoke@users.noreply.github.com>
2022-03-24 02:17:27 +0100

    added login and register to home page
    *abdou updated contact page 
    *hakim added login adn register buttons on home page 
    *section des produits updated et responsive 
    win yes3an golf 6 2020 tejini lpark

--- BACK/server.php
+++ BACK/server.php
@@ -7,7 +7,7 @@ $email    = "";
 $errors = array(); 
 
 // connect to the database
-$db = mysqli_connect('localhost', 'abdou', 'password', 'IHM');
+$db = mysqli_connect('localhost', 'root', 'password', 'IHM');
 
 // register user
 if (isset($_POST['reg_user'])) {
