f9fc757dadb1dcdb06499bb4be9565b0aaa9db24
Abderrahmane BENAISSA <76059937+Abdulrahman-02@users.noreply.github.com>
2022-07-05 23:40:15 +0100

    Delete profile directory

--- profile/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db = mysqli_connect('localhost', 'root', 'password', 'ihm');
-
-// Check connection
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
